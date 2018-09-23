33b4981fc5f4db621c4578d168b8615c7a6fe209
maur7098 <maur7098@dell>
2018-09-22 21:07:58 -0500

    added new python files

---
+++ blocks.py
@@ -0,0 +1,15 @@
+# Does line execute?
+#
+if 'foo' in ['foo', 'bar', 'baz']:
+    print('Outer condition is true')
+
+    if 10 > 20:
+        print('Inner condition 1')
+
+    print('Between inner conditions')
+
+    if 10 < 20:
+        print('Inner condition 2')
+
+    print('End of outer condition')
+print('After outer condition')
---
+++ foo.py
@@ -0,0 +1,6 @@
+if 'foo' in ['bar', 'baz', 'quz']:
+    print('Expression was true')
+    print('Executing statement in suite')
+    print('...')
+    print('Done.')
+print('After conditional')
