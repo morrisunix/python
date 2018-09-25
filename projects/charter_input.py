bd34f3d55979b111fa8e2913cdcff82c37ceb7c5
morrisunix <morrisunix@gmail.com>
2018-09-25 13:30:21 -0500

    new exercise working

--- projects/charter_input.py
+++ projects/charter_input.py
@@ -1,3 +1,22 @@
+from datetime import datetime
+
+def age_calculate(age):
+    """ Calculate the year when the age will turn 100 years old
+
+    Parameters
+    ----------
+    age: int
+        Current age
+
+    Returns
+    -------
+    string
+        The year when the age holder will turn 100 years old
+    """
+    current_year = datetime.now().year
+    remaining_years_to_100 = 100 - age
+    year_when_turn_100 = current_year + remaining_years_to_100
+    return year_when_turn_100
 
 
 def main():
@@ -11,6 +30,24 @@ def main():
     -------
     None
     """
+    while True:
+        name = input("Please provide your name: ")
+        age = input("Please provide your current age: ")
+        while not age.isnumeric():
+            print("That is not a valid integer number")
+            age = input("Please provide your current age: ")
+        loops = input("How many times you want to get the response printed: ")
+        while not loops.isnumeric():
+            print("That is not a valid integer number")
+            loops = input("How many times you want to get the response printed: ")
+        loops = int(loops)
+        year_when_turn_100 = age_calculate(int(age))
+        printed_output = "Hello {} you will turn 100 in {}! \n".format(name, year_when_turn_100)
+        printed_output *= loops
+        print(printed_output)
+        continue_loop = input("Try again Y/N?").upper()
+        if continue_loop == 'N':
+            break
 
 if __name__ == "__main__":
     main()
