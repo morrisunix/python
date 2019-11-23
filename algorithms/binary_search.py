b1ceee3deddd6e5493b790c7a0b8fd2f25451252
morrisunix <morrisunix@gmail.com>
2019-11-22 23:54:40 -0600

    Binary search

---
+++ algorithms/binary_search.py
@@ -0,0 +1,16 @@
+
+def search(number):
+    """ Returns if the nimber has been found on the array using a binary search
+
+    Parameters
+    ----------
+    number: int
+        The numebr you are searching
+
+    Returns
+    -------
+    bool
+        True if found, False if doesn't
+    """
+
+        
