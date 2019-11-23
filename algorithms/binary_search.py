195d39c571da542ffccdcb2849be679e0b413932
morrisunix <morrisunix@gmail.com>
2019-11-22 23:57:26 -0600

    Added main function

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -13,4 +13,17 @@ def search(number):
         True if found, False if doesn't
     """
 
-        
+def main():
+    """ Main function
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+
+if __name__ == "__main__":
+    main()
